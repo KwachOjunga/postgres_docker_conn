44b6d144307c72d2f1e17c896b2ea710bd2caa41
KwachOjunga <ojungakwach@gmail.com>
2024-12-24 15:52:50 +0300

    modified source

--- src/main.rs
+++ src/main.rs
@@ -31,43 +31,55 @@ fn list_user(conn: &mut Client) -> Result<Vec<(String, String)>, Error> {
     Ok(res)
 }
 
-
 #[derive(Parser)]
-#[command(version, about = "A cli that works as postgres client" )]
+#[command(version, about = "A cli that works as postgres client")]
 struct Cli {
-	///Sets an address of db connection
-	#[arg(short, long, value_name = "ADDR")]
-	db : Option<String>,
+    ///Sets an address of db connection
+    #[arg(short, long, value_name = "ADDR")]
+    db: Option<String>,
 
-	#[command(subcommand)]
-	command: Option<Commands>
-} 
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
 
 #[derive(Subcommand)]
 enum Commands {
-	/// add users to table
-	Add {
-		/// user's name
-		#[arg(long)]
-		name: String,
-		/// user's email
-		#[arg(long)]
-		email: String
-	},
-	/// create users table
-	Create,
-	/// list users in table
-	List
+    /// add users to table
+    Add {
+        /// user's name
+        #[arg(long)]
+        name: String,
+        /// user's email
+        #[arg(long)]
+        email: String,
+    },
+    /// create users table
+    Create,
+    /// list users in table
+    List,
 }
 
 fn main() {
-	let cli = Cli::parse();
-	let db = cli.db.as_deref().unwrap_or("postgres://postgres@localhost:8000");	
-	let mut conn = Client::connect(db, NoTls).unwrap();
-	match &cli.command {
-		Some(Commands::Create) => {create_table(&mut conn).unwrap();},
-		Some(Commands::Add { name, email }) => {create_user(&mut conn, name, email).unwrap();}
-		Some(Commands::List) => {println!("{:?}",list_user(&mut conn).unwrap());},
-		_ => /*println!("try using oe of the commands")*/{}
-	}
+    let cli = Cli::parse();
+    let db = cli
+        .db
+        .as_deref()
+        .unwrap_or("postgres://postgres@localhost:8000");
+    let mut conn = Client::connect(db, NoTls).unwrap();
+    match &cli.command {
+        Some(Commands::Create) => {
+            create_table(&mut conn).unwrap();
+        }
+        Some(Commands::Add { name, email }) => {
+            create_user(&mut conn, name, email).unwrap();
+        }
+        Some(Commands::List) => {
+            for (name, email) in list_user(&mut conn).unwrap() {
+                println!("The name {name:#?} and email {email:#?}");
+            }
+        }
+        _ =>
+            /*println!("try using oe of the commands")*/
+            {}
+    }
 }
